db0243ce12d4e29ba9d0f1d3075f7018b8f38eb0
Denis Feofanov <omail4me@gmail.com>
2019-02-21 18:23:52 +0300

    style: add headings

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -27,8 +27,20 @@ const COLUMNS = {
 
 const Stories = ({ stories }) => (
   <div className="stories">
+    <StoriesHeader columns={COLUMNS} />
+
     {(stories || []).map(story => (
       <Story key={story.objectID} story={story} columns={COLUMNS} />
+    ))}
+  </div>
+);
+
+const StoriesHeader = ({ columns }) => (
+  <div className="stories-header">
+    {Object.keys(columns).map(key => (
+      <span key={key} style={{ width: columns[key].width }}>
+        {columns[key].label}
+      </span>
     ))}
   </div>
 );
